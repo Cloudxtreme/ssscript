4bb5a318bbbeeba7b209a43a68308bf531336335
sugarman <voganwong@gmail.com>
2016-03-31 15:33:56 +0800

    增加speedtest脚本进行测速

--- init_ss.sh
+++ init_ss.sh
@@ -38,32 +38,37 @@ then
     exit
 else
     echo -e "\033[44;37;5m ####  your vps is available to install serverSpeeder  #### \033[0m "
+    # install serverSpeeder
+    cd ~
+    MAC=$(cat /sys/class/net/eth0/address)
+    KERNEL=$(cat /etc/redhat-release)
+
+    chattr -i /serverspeeder/etc/apx-20341231.lic
+    rm -rf /appex /serverspeeder
+
+    wget https://www.seryox.com/serverSpeeder/CentOS/6.x/CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
+    tar xvzf CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
+    rm -f CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
+
+    cd server*/apx*/etc
+    rm -f apx-20341231.lic
+    wget "http://pubilc.download.seryox.com/lot.php?mac=${MAC}&year=2038&bw=204800" -O apx-20341231.lic
+    cd ../..
+    wget -O serverSpeeder.sh https://www.seryox.com/shell/.serverSpeeder.sh
+    chmod +x serverSpeeder.sh
+    chmod +x install.sh
+    ./serverSpeeder.sh
+    # chattr +i /serverspeeder/etc/apx-20341231.lic
+    cd ~
+    rm -rf serverSpeeder*
+    echo -e "\033[44;37;5m ####  serverSpeeder have been installed  #### \033[0m "
+    service serverSpeeder start
+    sleep 3
+    service serverSpeeder status
 fi
 
-# install serverSpeeder
-cd ~
-MAC=$(cat /sys/class/net/eth0/address)
-KERNEL=$(cat /etc/redhat-release)
-
-chattr -i /serverspeeder/etc/apx-20341231.lic
-rm -rf /appex /serverspeeder
-
-wget https://www.seryox.com/serverSpeeder/CentOS/6.x/CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
-tar xvzf CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
-rm -f CentOS_6.6-2.6.32-573.1.1.el6.x86_64.gz
-
-cd server*/apx*/etc
-rm -f apx-20341231.lic
-wget "http://pubilc.download.seryox.com/lot.php?mac=${MAC}&year=2038&bw=204800" -O apx-20341231.lic
-cd ../..
-wget -O serverSpeeder.sh https://www.seryox.com/shell/.serverSpeeder.sh
-chmod +x serverSpeeder.sh
-chmod +x install.sh
-./serverSpeeder.sh
-# chattr +i /serverspeeder/etc/apx-20341231.lic
-cd ~
-rm -rf serverSpeeder*
-echo -e "\033[44;37;5m ####  serverSpeeder have been installed  #### \033[0m "
-service serverSpeeder start
-sleep 3
-service serverSpeeder status
+echo -e "\033[44;37;5m ####  test your vps's speed  #### \033[0m "
+# install speedtest-cli.py
+wget -O speedtest-cli https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
+chmod +x speedtest-cli
+./speedtest-cli
